592da37101ff40ed16c31ed652cddd82451cb1b3
JohnsonTW <chlitnc1986@gmail.com>
2019-10-30 10:51:37 +0800

    20191030

--- PH_offline_performance/ph_offline_performance.py
+++ PH_offline_performance/ph_offline_performance.py
@@ -63,7 +63,7 @@ class TimeTests(unittest.TestCase):
         self.driver.find_element_by_name("yt0").click()
 
     #Bank Statement - Detail Performance
-    # def test_00001_bsdetail(self):
+    def test_00001_bsdetail(self):
         settc1()
         settc2()
         self.driver.find_element_by_link_text("Reports").click()
@@ -79,20 +79,23 @@ class TimeTests(unittest.TestCase):
         self.driver.find_element_by_id("START_DATE").send_keys(sdate)
         self.driver.find_element_by_id("END_DATE").clear()
         self.driver.find_element_by_id("END_DATE").send_keys(edate)
-        select = Select(self.driver.find_element_by_name('BANK_ACCT_PROVIDER_ID'))
-        select.select_by_visible_text(provider)
-        #time.sleep(5)
-        self.driver.find_element_by_id("btn_Search").click()
         #Search前 先記錄時間
         sTime = time.time()
+        self.driver.find_element_by_id("btn_Search").click()     
+        show = self.driver.find_element_by_css_selector("#statement-detail-grid > div.summary").text
+        url_now = self.driver.current_url
 
         #有找到Table中的元素(view圖示),table出來才算搜尋結束        
         if self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/table/tbody/tr[1]/td[22]/a/img"):
             #Search後找到element,記錄時間
             eTime = time.time()
-            print("Search Condition: \n Start Date: "+ sdate + "\n" + "End Date: "+ edate + "\n" + "Provider: "+provider)
-            print("*** Bank Statement - Detail search cost " + str(Decimal(eTime - sTime).quantize(Decimal('0.00'))) + "seconds.***")
-        time.sleep(1)
+            total = eTime - sTime
+            print("Search Condition: \nStart Date: "+ sdate + "\n" + "End Date: "+ edate)
+            print("*** Bank Statement - Detail search cost " + str(Decimal(total).quantize(Decimal('0.00'))) + " seconds.***")
+            if total > 30.00:
+                print("Loading cost over 30sec!")
+        self.assertIn("report/bankStatement",url_now, msg="Wrong Page!")
+        self.assertIn("Displaying",show, msg="Display Error!")
     
     #Deposit Bank Statement Performance
     def test_00002_bsmana(self):
@@ -112,16 +115,25 @@ class TimeTests(unittest.TestCase):
         self.driver.find_element_by_id("END_DATE").clear()
         self.driver.find_element_by_id("END_DATE").send_keys(edate)
         self.driver.find_element_by_xpath("//*[@id='ui-datepicker-div']/div[3]/button[2]").click()
-        self.driver.find_element_by_id("btn_Search").click()
+        
         #Search前 先記錄時間
         sTime = time.time()
+        self.driver.find_element_by_id("btn_Search").click()
 
+        show = self.driver.find_element_by_css_selector("#file-list-grid > div.summary").text
+        url_now = self.driver.current_url
         #有找到Table中的元素(view圖示),table出來才算搜尋結束       
         if self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/table/tbody/tr[1]/td[18]/a/img"):
             #Search後找到element,記錄時間
             eTime = time.time()
+            total = eTime - sTime
             print("Search Condition: \n" + "Start Date: "+ sdate + "\n" + "End Date: "+ edate)
-            print("*** Deposit Bank Statement search cost " + str(Decimal(eTime - sTime).quantize(Decimal('0.00'))) + "seconds.***")
+            print("*** Deposit Bank Statement search cost " + str(Decimal(total).quantize(Decimal('0.00'))) + "seconds.***")
+            if total > 30.00:
+                print("Loading cost over 30sec!")    
+        self.assertIn("bankStatement/deposit",url_now, msg="Wrong Page!")
+        self.assertIn("Displaying",show, msg="Display Error!")
+        
         time.sleep(1)
     
     def test_00003_deposittran(self):
@@ -133,14 +145,24 @@ class TimeTests(unittest.TestCase):
         self.driver.find_element_by_id("START_DATE").send_keys(sdate)
         self.driver.find_element_by_id("END_DATE").clear()
         self.driver.find_element_by_id("END_DATE").send_keys(edate)
+        
+        #Search前 先記錄時間
+        sTime = time.time()
         self.driver.find_element_by_id("btn_Search").click()
 
-        sTime = time.time()
-
+        show = self.driver.find_element_by_css_selector("#statement-detail-grid > div.summary").text
+        url_now = self.driver.current_url    
+           
         if self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/table/tbody/tr[1]/td[25]/a/img"):
             eTime = time.time()
+            total = eTime - sTime
             print("Search Condition: \n" + "Start Date: "+ sdate+ "\n" +"End Date: "+ edate)
-            print("*** Deposit Transation search cost " + str(Decimal(eTime - sTime).quantize(Decimal('0.00'))) + "seconds.***")
+            print("*** Deposit Transation search cost " + str(Decimal(total).quantize(Decimal('0.00'))) + "seconds.***")
+            if total > 30.00:
+                print("Loading cost over 30sec!")
+        self.assertIn("/report/requestDetail/",url_now, msg="Wrong Page!")
+        self.assertIn("Displaying",show, msg="Display Error!")
+        
         time.sleep(1)
         
     #End the test, close the browser window
