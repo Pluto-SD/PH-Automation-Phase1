7c964f8c35121137db287889511df670e28e73ef
JohnsonTW <chlitnc1986@gmail.com>
2019-10-31 16:07:45 +0800

    20191031_apg_switch_page_time

---
+++ PH_offline_switchpage/ph_offline_switchpage_report.py
@@ -0,0 +1,28 @@
+import unittest
+import HTMLTestRunner
+import os
+from ph_offline_switchpage import switchpage
+
+#from homepagetests import HomePageTest
+
+# get the directory path to output report file
+result_dir = os.getcwd()
+
+# get all tests from SearchProductTest and HomePageTest class
+test_page = unittest.TestLoader().loadTestsFromTestCase(switchpage)
+#home_page_tests = unittest.TestLoader().loadTestsFromTestCase(HomePageTest)
+
+# create a test suite combining search_test and home_page_test
+#smoke_tests = unittest.TestSuite([home_page_tests, search_tests])
+#one_tests = unittest.TestSuite(test_Logout)
+
+# open the report file
+outfile = open(result_dir + '\PH_APG_Swtich_Page_Test.html', 'w')
+
+# configure HTMLTestRunner options
+runner = HTMLTestRunner.HTMLTestRunner(stream=outfile,
+                                       title='PH APG Swtich Page Test Report',
+                                       description='PH APG Swtich Page time Test')
+
+# run the suite using HTMLTestRunner
+runner.run(test_page)
