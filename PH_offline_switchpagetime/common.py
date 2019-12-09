0a532cbd8c720e2c1f7bd870929e5a8a51145998
JohnsonTW <chlitnc1986@gmail.com>
2019-12-09 16:41:15 +0800

    20121209_add common.py_統計所有case執行時間

---
+++ PH_offline_switchpagetime/common.py
@@ -0,0 +1,99 @@
+import os
+from selenium import webdriver
+
+class ph():
+    
+    def __init__(self):
+        self.TC1 = []
+        self.TC2 = []
+        self.TC3 = []
+        self.TC4 = []
+        self.TC5 = []
+        self.TC6 = []
+        self.TC7 = []
+
+    def readfile(self):
+        if os.path.isfile("switchpage.csv"):
+            print('找到目標檔案')
+            #讀取檔案
+            with open('switchpage.csv', 'r',encoding = 'utf-8', errors = "ignore") as f:
+                for line in f:
+                    s = line.strip().split(',') # split(',')就是遇到','就切一刀下去變成兩個欄位
+                    case = s[0]
+                    item = s[1]
+                    data = s[2]
+                    if case == 'login':
+                        self.TC1.append([item, data])
+                    elif case == 'time':
+                        self.TC2.append([item, data])
+                    elif case == "time2":
+                        self.TC3.append([item, data])
+                    elif case == "prov":
+                        self.TC4.append([item, data])
+                    elif case == "blank":
+                        self.TC5.append([item, data])
+                    elif case == "repeat":
+                        self.TC6.append([item, data])
+                    elif case == "invalid":
+                        self.TC7.append([item, data])
+
+    def webd(self):
+        acct     =  self.TC1[0][1]
+        pw       =  self.TC1[1][1]
+        name     =  self.TC1[2][1]
+        dir = os.getcwd()
+        ie_driver_path = dir + '\IEDriverServer.exe'
+        # create a new Internet Explorer session
+        driver = webdriver.Ie(ie_driver_path)
+        driver.implicitly_wait(360)
+        driver.maximize_window()
+        driver.get('https://pholadminsd.pd.local/admin/auth/login')
+        driver.get("javascript:document.getElementById('overridelink').click();") 
+        driver.find_element_by_id("UserLoginForm_username").clear()
+        driver.find_element_by_id("UserLoginForm_username").send_keys(acct)
+        driver.find_element_by_id("UserLoginForm_password").clear()
+        driver.find_element_by_id("UserLoginForm_password").send_keys(pw)
+        driver.find_element_by_name("yt0").click()
+
+        return driver
+
+    def webd_nologin(self):
+       
+        dir = os.getcwd()
+        ie_driver_path = dir + '\IEDriverServer.exe'
+        # create a new Internet Explorer session
+        driver = webdriver.Ie(ie_driver_path)
+        driver.implicitly_wait(360)
+        driver.maximize_window()
+        driver.get('https://pholadminsd.pd.local/admin/auth/login')
+        driver.get("javascript:document.getElementById('overridelink').click();") 
+        return driver
+
+    def login(self):  
+        return  self.TC1
+    
+    def looplogin(self):  
+        return  self.TC6
+
+    def blank(self):  
+        return  self.TC5
+
+    def invalid(self):  
+        return  self.TC7
+
+    def date_a(self):
+        global sdate,edate
+        sdate     = self.TC2[0][1]
+        edate     = self.TC2[1][1]
+        return self.TC2
+    
+    def date_b(self):
+        global sdate2,edate2
+        sdate2     = self.TC3[0][1]
+        edate2     = self.TC3[1][1]
+        return self.TC3
+
+    def provider(self):
+        global prov
+        prov     = self.TC4[0][1]
+        return self.TC4
--- PH_offline_switchpagetime/ph_offline_switchpagetime.py
+++ PH_offline_switchpagetime/ph_offline_switchpagetime.py
@@ -5,54 +5,21 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from decimal import *
+from common import ph
 
-TC1 = []
-TC2 = []
+read = ph()
+read.readfile()
 
-if os.path.isfile('switchpage.csv'):
-    print('找到目標檔案 switchpage.csv')
-    #讀取檔案
-    with open('switchpage.csv', 'r',encoding = 'utf-8', errors = "ignore") as f:
-        for line in f:
-			# if 'login' in line: continue #繼續,跳到下一迴()
-            s = line.strip().split(',') # split(',')就是遇到','就切一刀下去變成兩個欄位
-            case = s[0]
-            item = s[1]
-            data = s[2]
-            if case == 'login':
-                TC1.append([item, data])
-
-def settc1():
-    global acct, pw, name
-    acct     = TC1[0][1]
-    pw       = TC1[1][1]
-    name     = TC1[2][1]
-
-
+unittest.total = 0
 
 class switchpage(unittest.TestCase):
 
     #initiation for the test
     @classmethod
     def setUpClass(cls):
-        settc1()
-        dir = os.getcwd()
-        ie_driver_path = dir + '\IEDriverServer.exe'
-        # create a new Internet Explorer session
-        cls.driver = webdriver.Ie(ie_driver_path)
-        cls.driver.implicitly_wait(30)
-        cls.driver.maximize_window()
-        cls.driver.get('https://pholadminsd.pd.local/admin/auth/login')
-
-        #For IE Only
-        cls.driver.get("javascript:document.getElementById('overridelink').click();") 
-        #Login
-        cls.driver.find_element_by_id("UserLoginForm_username").clear()
-        cls.driver.find_element_by_id("UserLoginForm_username").send_keys(acct)
-        cls.driver.find_element_by_id("UserLoginForm_password").clear()
-        cls.driver.find_element_by_id("UserLoginForm_password").send_keys(pw)
-        cls.driver.find_element_by_name("yt0").click()
-
+        
+        cls.driver = read.webd()
+        
     #Swotch to Bank Statement - Detail time
     def test_00001_swbsdetail(self):
         
@@ -68,8 +35,10 @@ class switchpage(unittest.TestCase):
 
         if self.driver.find_element_by_css_selector("#content > h1"):
             eTime = time.time()
-            total = eTime - sTime
-            print("\n"+"Switch to Reports > Bank Statement > Bank Statement - Detail cost " + str(Decimal(total).quantize(Decimal('0.00'))) + " seconds." )
+            t01 = eTime - sTime
+            print("\n"+"Switch to Reports > Bank Statement > Bank Statement - Detail cost " + str(Decimal(t01).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t01
 
         url_now = self.driver.current_url
         path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(4)").text
@@ -93,8 +62,10 @@ class switchpage(unittest.TestCase):
 
         if self.driver.find_element_by_css_selector("#content > h1"):
             eTime = time.time()
-            total = eTime - sTime
-            print("\n"+"Switch to Reports > Bank Statement > Bank Statement - Summary cost " + str(Decimal(total).quantize(Decimal('0.00'))) + " seconds." )
+            t02 = eTime - sTime
+            print("\n"+"Switch to Reports > Bank Statement > Bank Statement - Summary cost " + str(Decimal(t02).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t02
 
         url_now = self.driver.current_url
         path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(4)").text
@@ -115,8 +86,10 @@ class switchpage(unittest.TestCase):
 
         if self.driver.find_element_by_css_selector("#content > h1"):
             eTime = time.time()
-            total = eTime - sTime
-            print("\n"+"Switch to Reports > Raw Bank Statement Ledger cost " + str(Decimal(total).quantize(Decimal('0.00'))) + " seconds." )
+            t03 = eTime - sTime
+            print("\n"+"Switch to Reports > Raw Bank Statement Ledger cost " + str(Decimal(t03).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t03
 
         url_now = self.driver.current_url
         path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
@@ -136,8 +109,10 @@ class switchpage(unittest.TestCase):
 
         if self.driver.find_element_by_css_selector("#content > h1"):
             eTime = time.time()
-            total = eTime - sTime
-            print("\n"+"Switch to Reports > Expire and Manual Accept Bank Record cost " + str(Decimal(total).quantize(Decimal('0.00'))) + " seconds." )
+            t04 = eTime - sTime
+            print("\n"+"Switch to Reports > Expire and Manual Accept Bank Record cost " + str(Decimal(t04).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t04
 
         url_now = self.driver.current_url
         path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
@@ -156,8 +131,10 @@ class switchpage(unittest.TestCase):
        
         if self.driver.find_element_by_css_selector("#content > h1"):
             eTime = time.time()
-            total = eTime - sTime
-            print("\n"+ "Switch to Reports > Unknown Bank Statement Detail cost " + str(Decimal(total).quantize(Decimal('0.00'))) + " seconds." )
+            t05 = eTime - sTime
+            print("\n"+ "Switch to Reports > Unknown Bank Statement Detail cost " + str(Decimal(t05).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t05
 
         url_now = self.driver.current_url
         path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
@@ -165,8 +142,209 @@ class switchpage(unittest.TestCase):
         self.assertIn("unknownBankStatement/index",url_now, msg="Wrong Page!")
         self.assertIn("Unknown Bank Statement Detail",path, msg="Display Error!")
         self.assertIn("Unknown Bank",title, msg="Display Error!")
-    #End the test, close the browser window
+    
+    #Deposit Transaction
+    def test_00006_swdepost(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Deposit Transaction").click()
        
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t06 = eTime - sTime
+            print("\n"+ "Switch to Reports > Deposit Transaction " + str(Decimal(t06).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t06
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("/report/requestDetail",url_now, msg="Wrong Page!")
+        self.assertIn("Deposit Transaction",path, msg="Display Error!")
+        self.assertIn("Deposit Transaction",title, msg="Display Error!")
+
+    #Payout Summary
+    def test_00007_swpayout(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Payout Summary").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t07 = eTime - sTime
+            print("\n"+ "Switch to Reports > Payout Summary " + str(Decimal(t07).quantize(Decimal('0.00'))) + " seconds." )
+        
+        unittest.total = unittest.total + t07
+        
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("report/payoutSummary",url_now, msg="Wrong Page!")
+        self.assertIn("Payout Summary",path, msg="Display Error!")
+        self.assertIn("Payout Summary",title, msg="Display Error!")   
+
+    #Payout Reconciliation Summary
+    def test_00008_swpayoutrecon(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Payout Reconciliation Summary").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t08 = eTime - sTime
+            print("\n"+ "Switch to Reports > Payout Reconciliation Summary " + str(Decimal(t08).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t08
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("report/payoutReconSummary",url_now, msg="Wrong Page!")
+        self.assertIn("Payout Reconciliation",path, msg="Display Error!")
+        self.assertIn("Payout Reconciliation Summary",title, msg="Display Error!")       
+    
+    #FxMark
+    def test_00009_swFxMark(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("FX Markup").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t09 = eTime - sTime
+            print("\n"+ "Switch to Reports > FX Markup " + str(Decimal(t09).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t09
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("/report/fxMarkup",url_now, msg="Wrong Page!")
+        self.assertIn("FX Markup",path, msg="Display Error!")
+        self.assertIn("FX Markup Report",title, msg="Display Error!")
+
+    #Pending Fund Balance
+    def test_00010_swPendingFund(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Pending Fund Balance").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t10 = eTime - sTime
+            print("\n"+ "Switch to Reports > Pending Fund Balance " + str(Decimal(t10).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t10
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("/report/pendingFundBal",url_now, msg="Wrong Page!")
+        self.assertIn("Pending Fund Balance",path, msg="Display Error!")
+        self.assertIn("Pending Fund Balance",title, msg="Display Error!")
+    
+    #Fee Summary
+    def test_00011_swFeeSummary(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Fee Summary").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t11 = eTime - sTime
+            print("\n"+ "Switch to Reports > Fee Summary " + str(Decimal(t11).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t11
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("/report/feeSummary",url_now, msg="Wrong Page!")
+        self.assertIn("Fee Summary",path, msg="Display Error!")
+        self.assertIn("Fee Summary",title, msg="Display Error!")        
+
+    #Total Fee Summary
+    def test_00012_swTotalFeeSummary(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Total Fee Summary").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t12 = eTime - sTime
+            print("\n"+ "Switch to Reports > Total Fee Summary " + str(Decimal(t12).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t12
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("/report/totalFeeSummary",url_now, msg="Wrong Page!")
+        self.assertIn("Total Fee Summary",path, msg="Display Error!")
+        self.assertIn("Total Fee Summary",title, msg="Display Error!")  
+
+    #Cost Summary
+    def test_00013_swCostSummary(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Cost Summary").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t13 = eTime - sTime
+            print("\n"+ "Switch to Reports > Cost Summary " + str(Decimal(t13).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t13
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("/report/costSummary",url_now, msg="Wrong Page!")
+        self.assertIn("Cost Summary",path, msg="Display Error!")
+        self.assertIn("Cost Summary",title, msg="Display Error!")
+    
+    #Total Cost Summary
+    def test_00014_swTotalCostSummary(self):   
+        #換頁前,先記錄時間
+        sTime = time.time()
+        self.driver.find_element_by_link_text("Reports").click()
+        #Bank Statement
+        self.driver.find_element_by_link_text("Total Cost Summary").click()
+       
+        if self.driver.find_element_by_css_selector("#content > h1"):
+            eTime = time.time()
+            t14 = eTime - sTime
+            print("\n"+ "Switch to Reports > Total Cost Summary " + str(Decimal(t14).quantize(Decimal('0.00'))) + " seconds." )
+
+        unittest.total = unittest.total + t14
+
+        url_now = self.driver.current_url
+        path = self.driver.find_element_by_css_selector("#page > div.breadcrumbs > span:nth-child(3)").text
+        title = self.driver.find_element_by_css_selector("#content > h1").text      
+        self.assertIn("/report/totalCostSummary",url_now, msg="Wrong Page!")
+        self.assertIn("Total Cost Summary",path, msg="Display Error!")
+        self.assertIn("Total Cost Summary",title, msg="Display Error!")
+
+    def test_00020_TotalTime(self):   
+        print("\n"+ "All case cost " + str(Decimal(unittest.total).quantize(Decimal('0.00'))) + " seconds." )
+
+    #End the test, close the browser window
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
